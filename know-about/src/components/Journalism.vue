01c14e1a31e020d2f169c4ba67852f1336b5e34b
1164596522 <1164596522@qq.com>
2018-11-26 23:58:47 +0800

    有待完善

---
+++ know-about/src/components/Journalism.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- 	<div v-html="datas" ref="container">
+		{{datas}}
+	</div> -->
+	<div></div>
+</template>
+
+<script>
+	export default {
+// 		data: function() {
+// 			return {
+// 				datas:null,
+// 			}
+// 		},
+		mounted(){
+			var thisURL = document.URL; 
+			var getval =thisURL.split('?')[1]; 
+			var showval= getval.split("=")[1];
+			window.location.href = "http://daily.zhihu.com/story/"+showval;
+			this.$router.push('/index');
+// 			this.axios({
+// 				method:'get',
+// 				url:'/api/api/4/news/'+showval
+// 			})
+// 			.then(response => {
+// 				this.datas = response.data.body;
+// 			})
+// 			.catch(error => {
+// 				console.log(error)
+// 			});
+// 			history.pushState(null,null,document.URL); 
+// 				window.addEventListener('popstate', function() { 
+// 				history.pushState(null, null,document.location.href="http://localhost:8080"); 
+// 			});
+		}
+	}
+</script>
